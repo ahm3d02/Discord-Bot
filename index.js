1cbcc47af84147f195e078f1792af127c81cd9e0
ahm3d02 <57738969+ahm3d02@users.noreply.github.com>
2020-06-21 19:26:07 -0400

    Dumb gitignore

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const discord = require("discord.js");
 const client = new discord.Client();
 const fs = require('fs');
 
-client.login(fs.readFileSync('.gitignore', 'utf8'));
+client.login(fs.readFileSync('token.txt', 'utf8'));
 
 client.on('ready', () => {
   console.log("Bot is running");
