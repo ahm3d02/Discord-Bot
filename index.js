3889d769ae6b496fa1b575109ec5da6c54561124
ahm3d02 <57738969+ahm3d02@users.noreply.github.com>
2020-06-21 12:01:03 -0400

    Bot is online with test commands

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const discord = require("discord.js");
 const client = new discord.Client();
+
+client.login("NzI0MDg5OTczNzgyODcyMTM0.Xu-Cyg.CTeUsdxW4nj1AJfhabvGVCRkd_0");
+
+client.on('ready', () => {
+  console.log("Bot is running");
+});
+
+client.on('message', msg => {
+  if (msg.content == "HiBot") {
+    msg.reply("Hi");
+  }
+});
