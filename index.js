0ada9da6d4e0202e6c402a527caeca99a4a7dce2
ahm3d02 <57738969+ahm3d02@users.noreply.github.com>
2020-06-21 21:46:58 -0400

    Starting payload

--- index.js
+++ index.js
@@ -11,11 +11,15 @@ client.once('ready', () => {
 
 client.on('message', msg => {
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-
-  // console.log(`[SERVER] ${msg.author.username}: ${msg.content}`);
+  // console.log(msg.guild.roles);
+  // for (let role of msg.guild.roles.cache) {
+  //   console.log(role);
+  // }
+  console.log(`[SERVER] ${msg.author.username}: ${msg.content}`);
   let args = msg.content.slice(prefix.length).split(/ +/);
   const command = args.shift().toLowerCase();
   if(command == "hi") {
     msg.channel.send(`Hi <@${msg.author.id}>`);
+  //   if (msg.author.id === )
   }
 });
