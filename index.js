3d8e9f2ec4740837821724a3be922d577be277cd
ahm3d02 <57738969+ahm3d02@users.noreply.github.com>
2020-06-21 21:11:41 -0400

    Understanding discord js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const discord = require("discord.js");
 const client = new discord.Client();
 const fs = require('fs');
+let prefix = '?';
 
 client.login(process.env.token || fs.readFileSync('token.txt', 'utf8'));
 
-client.on('ready', () => {
-  console.log("Bot is running");
+client.once('ready', () => {
+  console.log("[SERVER] Bot is running");
 });
 
 client.on('message', msg => {
-  if (msg.content == "HiBot") {
-    msg.reply("Hi");
+  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
+
+  // console.log(`[SERVER] ${msg.author.username}: ${msg.content}`);
+  let args = msg.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  if(command == "hi") {
+    msg.channel.send(`Hi <@${msg.author.id}>`);
   }
 });
