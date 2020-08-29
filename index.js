df0832d040db47b8a7da059803af924ce6dca47e
ahm3d02 <57738969+ahm3d02@users.noreply.github.com>
2020-08-29 13:12:19 -0400

    Removed node_modules

--- index.js
+++ index.js
@@ -10,16 +10,11 @@ client.once('ready', () => {
 });
 
 client.on('message', msg => {
-  if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-  // console.log(msg.guild.roles);
-  // for (let role of msg.guild.roles.cache) {
-  //   console.log(role);
-  // }
-  console.log(`[SERVER] ${msg.author.username}: ${msg.content}`);
-  let args = msg.content.slice(prefix.length).split(/ +/);
-  const command = args.shift().toLowerCase();
-  if(command == "hi") {
-    msg.channel.send(`Hi <@${msg.author.id}>`);
-  //   if (msg.author.id === )
+  if (msg.content.toLowerCase() === "clutch") {
+    if (!msg.member.voiceChannel) {
+      msg.channel.send("You must be in a voice channel");
+      return;
+    }
   }
+  else if (!msg.content.startsWith(prefix) || msg.author.bot) return;
 });
